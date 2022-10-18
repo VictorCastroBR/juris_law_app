550f7abd75d634934aa72e13a0820bbe51730d60
victor castro <victor.castro@waybe.com.br>
2022-10-18 01:53:37 -0300

    Fixed ExpansionAreaWidget

--- lib/view/menuPage.dart
+++ lib/view/menuPage.dart
@@ -19,42 +19,75 @@ class MenuPageState extends State<MenuPage> {
         title: Text('JurisLaw'),
       ),
       body: Container(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                ClipOval(
-                  child: Container(
-                    width: 100,
-                    height: 100,
-                    child: Image.asset(
-                      'lib/src/images/henrycavill.jpg',
+        child: Padding(
+          padding: EdgeInsets.only(top: 40.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  ClipOval(
+                    child: Container(
+                      width: 100.0,
+                      height: 100.0,
+                      child: Image.asset(
+                        'lib/src/images/henrycavill.jpg',
+                      ),
                     ),
                   ),
-                ),
-                Text('Admin')
-              ],
-            ),
-            ExpansionPanelList(
-              expansionCallback: (int index, bool isExpanded) {},
-              children: [
-                ExpansionPanel(
-                  headerBuilder: (BuildContext context, bool isExpanded) {
-                    return ListTile(
-                      title: Text('Áreas de Interesse'),
-                    );
-                  },
-                  body: ListTile(
-                    title: Text('Teste'),
-                    subtitle: Text('Subtext'),
-                  ),
-                  isExpanded: true,
-                ),
-              ],
-            ),
-          ],
+                  Padding(
+                    padding: EdgeInsets.only(left: 20.0),
+                    child: Column(
+                      children: [
+                        Text('Admin'),
+                        Text('Escritório'),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Container(height: 30), // Space
+              ExpansionAreaWidget(),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class ExpansionAreaWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(5.0),
+      child: ExpansionTile(
+        initiallyExpanded: true,
+        title: Text('Áreas de interesse'),
+        children: [
+          ListTile(
+            leading: Icon(Icons.balance_outlined),
+            title: Text('D. Civil'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.balance_outlined),
+            title: Text('D. Penal'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.balance_outlined),
+            title: Text('D. Previdênciário'),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DivTeamWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('...');
   }
 }
