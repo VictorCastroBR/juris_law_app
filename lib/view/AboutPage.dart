63f5bdb0bd21f43a7296ce546a758be6ca9a56e6
vcarvalho1 <vihcarvalho@outlook.com>
2022-10-19 22:38:14 -0300

    about1

--- lib/view/AboutPage.dart
+++ lib/view/AboutPage.dart
@@ -21,24 +21,87 @@ class AboutPageState extends State<AboutPage> {
         title: Text(
           'Sobre',
         ),
-        backgroundColor: Color.fromARGB(255, 65, 121, 100),
       ),
-      backgroundColor: Color(0xFF7AAA98),
-      body: Container(
-        child: Column(
-          children: [
-            Text(
-              'O Jurislaw é um aplicativo de pesquisa jurisprudencial. \n'
-              'Seu objetivo é unificar jurisprudências de diversos tribunais do Brasil em uma única ferramenta e notificar novas decisões.',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold, 
+      body: ListView(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                  Color.fromARGB(255, 233, 223, 223),
+                  Color.fromARGB(255, 112, 182, 106)
+                ])),
+            child: Container(
+              width: double.infinity,
+              height: 350.0,
+              child: Center(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        DevProfile(
+                          name: "Viviane Alves",
+                          image: " ",
+                        ),
+                        DevProfile(
+                          name: "Victor Castro",
+                          image: " ",
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'O Jurislaw é um aplicativo de pesquisa jurisprudencial. \n'
+                      'Seu objetivo é unificar jurisprudências de diversos tribunais do Brasil em uma única ferramenta e notificar novas decisões.',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-        ),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class DevProfile extends StatelessWidget {
+  final String name;
+  final String image;
+
+  const DevProfile({super.key, this.name = "", this.image = ""});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        CircleAvatar(
+          backgroundImage: NetworkImage(
+            image,
+          ),
+          radius: 50.0,
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          name,
+          style: TextStyle(
+            fontSize: 22.0,
+            color: Colors.white,
+          ),
+        ),
+      ],
     );
   }
 }
