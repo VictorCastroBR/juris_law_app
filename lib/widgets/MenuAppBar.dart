59c83d201385ff468afa7ea28b7c0a4a45c1c319
victor castro <victor.castro@waybe.com.br>
2022-10-20 00:57:54 -0300

    Modal Sair

--- lib/widgets/MenuAppBar.dart
+++ lib/widgets/MenuAppBar.dart
@@ -1,12 +1,15 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'ModalLogoutWidget.dart';
 
 class MenuAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
-        children: [
+      child: Column(children: [
         UserAccountsDrawerHeader(
           decoration: BoxDecoration(color: Color.fromARGB(255, 140, 228, 185)),
           currentAccountPicture: ClipOval(
@@ -18,16 +21,12 @@ class MenuAppBar extends StatelessWidget {
         ListTile(
           leading: Icon(Icons.home),
           title: Text('Home'),
-          onTap: () {
-            Navigator.of(context).pushNamed('/menu');
-          },
+          onTap: () => Navigator.of(context).pushNamed('/menu'),
         ),
         ListTile(
           leading: Icon(Icons.info),
           title: Text('Sobre'),
-          onTap: () {
-            Navigator.of(context).pushNamed('/sobre');
-          },
+          onTap: () => Navigator.of(context).pushNamed('/sobre'),
         ),
         ListTile(
           leading: Icon(Icons.search),
@@ -37,12 +36,39 @@ class MenuAppBar extends StatelessWidget {
         ListTile(
           leading: Icon(Icons.calendar_month),
           title: Text('Atribuições'),
-          onTap: () {},
+          onTap: () => Navigator.of(context).pushNamed('/atribuicoes'),
         ),
         ListTile(
           leading: Icon(Icons.notifications_active),
           title: Text('Notificações'),
           onTap: () {},
+        ),
+        ListTile(
+          leading: Icon(Icons.logout, color: Colors.red),
+          title: Text('Sair'),
+          onTap: () => showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: const Text('Você deseja sair?'),
+              actions: <Widget>[
+                TextButton(
+                    onPressed: () => Navigator.pop(context, 'Cancel'),
+                    child: const Text('Cancel',
+                        style: TextStyle(color: Colors.white)),
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Color.fromARGB(255, 255, 0, 0)))),
+                TextButton(
+                    onPressed: () =>
+                        Navigator.of(context).pushReplacementNamed('/'),
+                    child: const Text('Sim',
+                        style: TextStyle(color: Colors.white)),
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Color.fromARGB(255, 0, 255, 4)))),
+              ],
+            ),
+          ),
         ),
       ]),
     );
